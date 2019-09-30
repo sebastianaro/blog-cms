fa5d56d5767794955fa7ff460c849113fe04bef8
sebastianaro <sebastianaro@gmail.com>
2019-09-30 19:42:16 -0400

    Update index.php
    Moved back to where I started. Trying to get to work.

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
- <?php 
- include_once 'assets/db_connect.php'
- ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,61 +16,18 @@
   		
 	</header>
 	<?php
-    function getPostDetailsFromDatabase() {
-    // Get the post title
-    $postTitle = rawurldecode($_GET["title"]);
-
-    // Get the post that matches the postTitle
-    include_once 'assets/db_connect.php';
-    $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
-    $result = mysqli_query($conn, $sql);
-
-    // Get the first row from the result as an associative array
-    $postDetails = mysqli_fetch_assoc($result);
-    return $postDetails;
-}
+    function getPostTitlesFromDatabase(){
+    	$postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+    	return $postTitles;
+    }
 
 	?>
 	<main>
-
-
-
-		 <?php
-
-         $sql = "SELECT title FROM posts";
-	     $result = mysqli_query($conn, $sql);
-	     $resultCheck = mysqli_num_rows($result);
-
-	     if (resultCheck > 0) {
-	     	while ($row = mysqli_fetch_assoc($result)){
-	     		echo $row['title']);
-	     	}
-	     }
-
-        ?>
 		<h1>Sebas's Blog</h1>
-        
 
 		<ul>
 			<?php
-
-			function getPostTitlesFromDatabase() {
-			    // Get all the post titles from the posts table
-			    include_once 'assets/db_connect.php';
-			    $sql = "SELECT title FROM posts";
-			    $result = mysqli_query($conn, $sql);
-			    
-			    // Get each result row as an assoc array, then add title to $postTitles
-			    $postTitles = array();
-			    while($row = mysqli_fetch_assoc($result)){
-			        array_push($postTitles, $row['title']);
-			    }
-			    return $postTitles;
-			}
 			$postTitles = getPostTitlesFromDatabase();
-
-
-
 
 			foreach ($postTitles as $postTitle)  {
                echo "<li><a href='post.php'>" . $postTitle . "</a></li>";
