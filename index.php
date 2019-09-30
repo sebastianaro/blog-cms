f2b8145ece602d99b36b9eb3510b1a6d4a882fdf
sebastianaro <sebastianaro@gmail.com>
2019-09-30 18:59:59 -0400

    Trying to connect to DB
    Can't seem to make it work. Still trying

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+ <?php 
+ include_once 'assets/db_connect.php'
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,10 +20,19 @@
   		
 	</header>
 	<?php
-    function getPostTitlesFromDatabase(){
-    	$postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
-    	return $postTitles;
-    }
+    function getPostDetailsFromDatabase() {
+    // Get the post title
+    $postTitle = rawurldecode($_GET["title"]);
+
+    // Get the post that matches the postTitle
+    include_once 'assets/db_connect.php';
+    $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
+    $result = mysqli_query($conn, $sql);
+
+    // Get the first row from the result as an associative array
+    $postDetails = mysqli_fetch_assoc($result);
+    return $postDetails;
+}
 
 	?>
 	<main>
@@ -28,6 +41,21 @@
 		<ul>
 			<?php
 			$postTitles = getPostTitlesFromDatabase();
+
+
+function getPostTitlesFromDatabase() {
+    // Get all the post titles from the posts table
+    include_once 'db_connect.php';
+    $sql = "SELECT title FROM posts";
+    $result = mysqli_query($conn, $sql);
+    
+    // Get each result row as an assoc array, then add title to $postTitles
+    $postTitles = array();
+    while($row = mysqli_fetch_assoc($result)){
+        array_push($postTitles, $row['title']);
+    }
+    return $postTitles;
+}
 
 			foreach ($postTitles as $postTitle)  {
                echo "<li><a href='post.php'>" . $postTitle . "</a></li>";
